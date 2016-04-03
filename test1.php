d65c2ca8f29457c93b814e15ec4cef868d5025e3
Shawn Gao <xxlangdou@gmail.com>
2016-04-03 04:26:20 -0500

    test

---
+++ test1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShawnG
+ * Date: 2016/4/3
+ * Time: 4:25
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+</head>
+<body>
+$END$
+</body>
+</html>
